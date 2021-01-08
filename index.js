fdb7993e1655a95044de77eb43cc7190c69a3364
Zach Smith <znathanielsmith@gmail.com>
2021-01-07 21:05:45 -0500

    added api call

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+function getStocks(value) {
+  console.log(value);
+  $.get(
+    `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${value}&apikey=DCJ1QDASJFVH1S53`,
+    function (data) {
+      console.log(data);
+    }
+  );
+}
+
+$(function () {
+  $("#searchButton").click(function (event) {
+    event.preventDefault();
+    let value = $("#inputValue").val();
+    getStocks(value);
+  });
+});
