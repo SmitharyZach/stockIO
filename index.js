05f6f7013689ca3b1dba03f539acb1b0048ceacf
andremikal <andremikalfuller@gmail.com>
2021-01-09 10:49:12 -0500

    Added remove

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 function getStocks(value) {
-  let $container = $(".container-fluid");
-  $.get(
-    `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${value}&apikey=DCJ1QDASJFVH1S53`,
-    function (data) {
-      data.bestMatches.forEach((element) => {
-        let symbol = element["1. symbol"];
-        $container.append(`<div>${symbol}</div>`);
-      });
-    }
-  );
+    let $container = $(".container-fluid");
+    $('.stockItems').remove();
+    $.get(
+        `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${value}&apikey=DCJ1QDASJFVH1S53`,
+        function (data) {
+            data.bestMatches.forEach((element) => {
+                let symbol = element["1. symbol"];
+                $container.append(`<div id=${symbol} class='stockItems'>${symbol}</div>`);
+            });
+        }
+    );
+    $('.stockItems').on('click', function () {
+        console.log('click');
+    })
 }
 
 $(function () {
-  $("#searchButton").click(function (event) {
-    event.preventDefault();
-    let value = $("#inputValue").val();
-    getStocks(value);
-  });
+    $("#searchButton").click(function (event) {
+        event.preventDefault();
+        let value = $("#inputValue").val();
+        getStocks(value);
+    });
 });
