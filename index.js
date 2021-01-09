8a3877606e9adfdf3922dbef7319794b4f978fab
Zach Smith <znathanielsmith@gmail.com>
2021-01-09 10:21:15 -0500

    add rendering

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function getStocks(value) {
-  console.log(value);
+  let $container = $(".container-fluid");
   $.get(
     `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${value}&apikey=DCJ1QDASJFVH1S53`,
     function (data) {
-      console.log(data);
+      data.bestMatches.forEach((element) => {
+        let symbol = element["1. symbol"];
+        $container.append(`<div>${symbol}</div>`);
+      });
     }
   );
 }
